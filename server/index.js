01dac5861ed4c5929a192890658a5a70463e30f7
Chineze Nwosu <chineze.nwosu@andela.com>
2017-10-27 15:24:40 +0100

    add index route to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ var express = require('express')
 var app = express()
 var port = 8080
 
+app.get('/', function(req, res) {
+  res.json({
+    name: 'Chineze'
+  })
+})
+
 app.listen(port, function() {
   console.log('Listening on port', port)
 })
