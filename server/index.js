401bf0b596f7c59b18bd19c852f5232f78c06f5d
Chineze Nwosu <chineze.nwosu@andela.com>
2017-10-27 15:39:13 +0100

    render react html from express server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 var port = 8080
 
+app.use(express.static(path.join(__dirname, '../public')))
+
 app.get('/', function(req, res) {
-  res.json({
-    name: 'Chineze'
-  })
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.listen(port, function() {
